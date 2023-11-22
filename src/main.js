f3ab6118b51ca2325f76af4fa769e5169160a318
divorahim13 <113409028+divorahim13@users.noreply.github.com>
2023-11-22 23:56:11 +0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 const hamburger = document.getElementById('hamburger');
 const navMenu = document.getElementById('nav-menu');
 const close = document.getElementById('tutup');
+const navLink = document.querySelectorAll('.nav__link');
+
+navLink.forEach(link =>
+    link.addEventListener("click", () => {
+        navMenu.classList.add('hidden')
+    })    
+)
 
 hamburger.addEventListener("click", () => {
     navMenu.classList.remove('hidden')
@@ -8,4 +15,51 @@ hamburger.addEventListener("click", () => {
 
 tutup.addEventListener('click', () => {
     navMenu.classList.add('hidden')
+})
+
+const tabs = document.querySelectorAll(".tabs_wrap ul li");
+const all = document.querySelectorAll('.item_wrap');
+const foods = document.querySelectorAll('.food');
+const beverages = document.querySelectorAll('.beverage');
+const snacks = document.querySelectorAll('.snack');
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+        tabs.forEach(tab => {
+            tab.classList.remove("active")
+        })
+        tab.classList.add('active')
+
+
+
+        const tabval = tab.getAttribute('data-tabs')
+
+
+        all.forEach(item => {
+            item.style.display = 'none'
+        })
+
+        if (tabval == 'food') {
+            foods.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+        else if (tabval == 'snack') {
+            snacks.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+        else if (tabval == 'beverage') {
+            beverages.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+        else {
+            all.forEach(item => {
+                item.style.display = 'block'
+            })
+        }
+            
+
+    })
 })
